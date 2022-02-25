41a91a1480d576f603ee11655ae81f38a389d518
Agustina Marijanovic <Agustina.Marijanovic@endava.com>
2022-02-25 11:01:07 -0300

    fix: remove unnecessary divs and fav button

--- src/components/DrinksList.jsx
+++ src/components/DrinksList.jsx
@@ -20,16 +20,12 @@ const DrinksList = ({ loading, error, drinks }) => {
   }
 
   return (
-    <div>
-      <div className="max-w-2xl mx-auto py-8 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        {/* el titulo podria ser dinamico, por ejemplo tragos con vodka */}
-        <h2 className="text-lg font-bold">Cocktails</h2>
-
-        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {drinks.map((drink) => (
-            <Drink key={drink.idDrink} drink={drink} />
-          ))}
-        </div>
+    <div className="max-w-2xl mx-auto py-8 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+      <h2 className="text-lg font-bold">Cocktails</h2>
+      <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        {drinks.map((drink) => (
+          <Drink key={drink.idDrink} drink={drink} />
+        ))}
       </div>
     </div>
   );
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -42,73 +42,56 @@ const Details = () => {
   ingredientsAndMeasures = ingredientsAndMeasures.filter(
     (obj) => obj.ingredient !== null,
   );
-
   return (
-    <div>
-      <section className="text-neutral-400 overflow-hidden">
-        <div className="container px-5 py-24">
-          <div className="lg:w-4/5 mx-auto flex flex-wrap">
-            <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-">
-              <div className="flex justify-between">
-                <div>
-                  <h2 className="text-sm text-neutral-500 tracking-widest">
-                    {drink.strCategory}
-                  </h2>
-                  <h1 className="text-white text-3xl font-medium mb-4">
-                    {drink.strDrink}
-                  </h1>
-                </div>
-                <button className="rounded-full w-10 h-10 bg-neutral-800 p-0 border-0 inline-flex items-center justify-center text-neutral-500 ml-4">
-                  <svg
-                    fill="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                  </svg>
-                </button>
-              </div>
-
+    <section className="text-neutral-400 overflow-hidden">
+      <div className="container px-5 py-24">
+        <div className="lg:w-4/5 mx-auto flex flex-wrap">
+          <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-">
+            <div className="flex justify-between">
               <div>
-                <h2 className="text-md text-neutral-100 tracking-widest pb-2">
-                  Instructions
+                <h2 className="text-sm text-neutral-500 tracking-widest">
+                  {drink.strCategory}
                 </h2>
-                <p className="leading-relaxed mb-4">{drink.strInstructions}</p>
-              </div>
-              <div>
-                <h2 className="text-md text-neutral-100 tracking-widest pb-2">
-                  Ingredients
-                </h2>
-                {ingredientsAndMeasures.map((ingredientObject) => (
-                  <Ingredients
-                    key={ingredientObject.ingredient}
-                    ingredient={ingredientObject.ingredient}
-                    measure={ingredientObject.measure ?? 'Up to you'}
-                  />
-                ))}
-
-                <div className="pt-4">
-                  <span className="text-md text-neutral-100 tracking-widest pb-2 mr-2">
-                    Glass
-                  </span>
-                  <span className="ml-auto text-neutral-500">
-                    {drink.strGlass}
-                  </span>
-                </div>
+                <h1 className="text-white text-3xl font-medium mb-4">
+                  {drink.strDrink}
+                </h1>
               </div>
             </div>
-            <img
-              alt="drink"
-              className="lg:w-1/2 w-full lg:h-auto object-cover object-center rounded"
-              src={drink.strDrinkThumb}
-            />
+            <div>
+              <h2 className="text-md text-neutral-100 tracking-widest pb-2">
+                Instructions
+              </h2>
+              <p className="leading-relaxed mb-4">{drink.strInstructions}</p>
+            </div>
+            <div>
+              <h2 className="text-md text-neutral-100 tracking-widest pb-2">
+                Ingredients
+              </h2>
+              {ingredientsAndMeasures.map((ingredientObject) => (
+                <Ingredients
+                  key={ingredientObject.ingredient}
+                  ingredient={ingredientObject.ingredient}
+                  measure={ingredientObject.measure ?? 'Up to you'}
+                />
+              ))}
+              <div className="pt-4">
+                <span className="text-md text-neutral-100 tracking-widest pb-2 mr-2">
+                  Glass
+                </span>
+                <span className="ml-auto text-neutral-500">
+                  {drink.strGlass}
+                </span>
+              </div>
+            </div>
           </div>
+          <img
+            alt="drink"
+            className="lg:w-1/2 w-full lg:h-auto object-cover object-center rounded"
+            src={drink.strDrinkThumb}
+          />
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
